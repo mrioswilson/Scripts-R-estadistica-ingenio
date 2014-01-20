0be1a534b984106998c164e0058535e9091e781a
mrioswilson <martinrioswilson@gmail.com>
2014-01-20 11:12:42 -0300

    Actividad 1 Fotorradares

--- fotorradares1.R
+++ fotorradares1.R
@@ -32,6 +32,13 @@ for (i in seq(along=lic)) {
 	if (lic[i]>1|lic[i]<0 ) { 
 		lic[i] <- 0} } }  # Arreglar muestra para dejar 1s y 0s.
 
+lic <- sort(lic);
+licnum <- sort(lic);
+if (identical(lic,rep(0,n))) { lic <- factor(lic,labels=c('Sin licencia')) } #Arreglar error cuando son todos iguales.
+else if (identical(lic,rep(1,n))) { lic <- factor(lic, labels=c('Con licencia'))} #Arreglar error cuando son todos iguales.
+else {lic <- factor(lic, labels=c('Sin licencia', 'Con licencia'))} #Vector con frecuencia porcentual de variable sexo.
+porlic <- c(round((length(which(licnum==1))/n)*100),round((length(which(licnum<1))/n)*100)); #Vector con frecuencia porcentual de variable sexo
+
 edad <- round(runif(n,lmin,lmax)); sexo <- trunc(rnorm(n,1,0.5)); phogar <- round(rnorm(n,3,0.5)); lhogar <- round(rnorm(n,2,0.5)); chogar <- round(rnorm(n,2,0.5)); vhogar <- round(rnorm(n,2,0.5));
 
 for (i in seq(along=sexo)){  ##Arreglando la variable sexo para que queden 1s y 0s
@@ -53,11 +60,23 @@ if (identical(sexonum,rep(0,n))) { sexocat <- factor(sexonum, labels=c("Masculin
 else if (identical(sexonum,rep(1,n))) { sexocat <- factor(sexonum, labels=c("Femenino"))} #Arreglar error cuando son todos iguales.
 else {sexocat <- factor(sexonum, labels=c("Masculino", "Femenino"))}; #Creacion de variable categórica para la variable sexo
 porsex <- c(round((length(which(sexonum==1))/n)*100),round((length(which(sexonum<1))/n)*100)); #Vector con frecuencia porcentual de variable sexo
+frecsex <- porsex/100; #Vector con frecuencia absoluta de variable sexo
 
 ##Creación de data frame (Actividad 2)
 datos <- data.frame(EDAD=edad, SEXO=sexocat, LICENCIA=lic, HABITANTESHOGAR=phogar, LICENCIAHOGAR=lhogar, CONDUCTORESHOGAR=chogar, VEHICULOSHOGAR=vhogar);
 
-##Display de datos
+#Creación de tablas de frecuencia y display (Actividad 2)
+
+fedad <- as.data.frame(table(edad)); fedad <- transform(fedad, Acfreq=cumsum(Freq), Abfreq=prop.table(Freq)); fedad <- transform(fedad, PorFrec=Abfreq*100); fedad <- transform(fedad, Ac.PorFrec=cumsum(PorFrec)); names(fedad) <- c('Edad', 'Frec', 'Ac. Frec', 'Abs. Frec', '% Frec', 'Ac. %Frec'); print(fedad);  
+fsex <- as.data.frame(table(sexocat)); fsex <- transform(fsex, Acfreq=cumsum(Freq), Abfreq=prop.table(Freq)); fsex <- transform(fsex, PorFrec=Abfreq*100); fsex <- transform(fsex, Ac.PorFrec=cumsum(PorFrec)); names(fsex) <- c('Sexo', 'Frec', 'Ac. Frec', 'Abs. Frec', '% Frec', 'Ac. %Frec'); print(fsex);
+fphogar <- as.data.frame(table(phogar)); fphogar <- transform(fphogar, Acfreq=cumsum(Freq), Abfreq=prop.table(Freq)); fphogar <- transform(fphogar, PorFrec=Abfreq*100); fphogar <- transform(fphogar, Ac.PorFrec=cumsum(PorFrec)); names(fphogar) <- c('Personas Hogar', 'Frec', 'Ac. Frec', 'Abs. Frec', '% Frec', 'Ac. %Frec'); print(fphogar); 
+flhogar <-as.data.frame(table(lhogar)); flhogar <- transform(flhogar, Acfreq=cumsum(Freq), Abfreq=prop.table(Freq)); flhogar <- transform(flhogar, PorFrec=Abfreq*100); flhogar <- transform(flhogar, Ac.PorFrec=cumsum(PorFrec)); names(flhogar) <- c('Licencia Hogar', 'Frec', 'Ac. Frec', 'Abs. Frec', '% Frec', 'Ac. %Frec'); print(flhogar);
+fchogar <- as.data.frame(table(chogar)); fchogar <- transform(fchogar, Acfreq=cumsum(Freq), Abfreq=prop.table(Freq)); fchogar <- transform(fchogar, PorFrec=Abfreq*100); fchogar <- transform(fchogar, Ac.PorFrec=cumsum(PorFrec)); names(fchogar) <- c('Conductores Hogar', 'Frec', 'Ac. Frec', 'Abs. Frec', '% Frec', 'Ac. %Frec'); print(fchogar);
+fvhogar <- as.data.frame(table(vhogar)); fvhogar <- transform(fvhogar, Acfreq=cumsum(Freq), Abfreq=prop.table(Freq)); fvhogar <- transform(fvhogar, PorFrec=Abfreq*100); fvhogar <- transform(fvhogar, Ac.PorFrec=cumsum(PorFrec)); names(fvhogar) <- c('Vehiculos Hogar', 'Frec', 'Ac. Frec', 'Abs. Frec', '% Frec', 'Ac. %Frec'); print(fvhogar);
+flic <- as.data.frame(table(lic)); flic <- transform(flic, Acfreq=cumsum(Freq), Abfreq=prop.table(Freq)); flic <- transform(flic, PorFrec=Abfreq*100); flic <- transform(flic, Ac.PorFrec=cumsum(PorFrec)); names(flic) <- c('Licencia', 'Frec', 'Ac. Frec', 'Abs. Frec', '% Frec', 'Ac. %Frec'); print(flic);
+
+
+##Display de datos (Actividad 2)
 print(datos);
 
 ##Calculo de datos actividad 4.
@@ -104,26 +123,39 @@ print(vhogar4);
 
 ##Exportar tabla de datos actividad 2
 
-write.table(datos, file='simulacion.xls', sep="\t", row.names=FALSE)
+library(xlsx); # Cargar paquete xlsx
+
+write.xlsx(datos, file='Datos actividad 2.xls', sheet='Datos', row.names=FALSE); #Escribir pestaña datos.
+write.xlsx(fedad, file='Datos actividad 2.xls', sheet='Edad', row.names=FALSE, append=TRUE); #Escribir pestaña edad.
+write.xlsx(fsex, file='Datos actividad 2.xls', sheet='Sexo', row.names=FALSE, append=TRUE); #Escribir pestaña sexo.
+write.xlsx(flic, file='Datos actividad 2.xls', sheet='Licencia', row.names=FALSE, append=TRUE); #Escribir pestaña licencia.
+write.xlsx(fphogar, file='Datos actividad 2.xls', sheet='Personas por hogar', row.names=FALSE, append=TRUE); #Escribir pestaña personas por hogar.
+write.xlsx(flhogar, file='Datos actividad 2.xls', sheet='Licencia por hogar', row.names=FALSE, append=TRUE); #Escribir pestaña licencia por hogar.
+write.xlsx(fchogar, file='Datos actividad 2.xls', sheet='Conductores por hogar', row.names=FALSE, append=TRUE); #Escribir pestaña conductores por hogar.
+write.xlsx(fvhogar, file='Datos actividad 2.xls', sheet='Vehiculos por hogar', row.names=FALSE, append=TRUE); #Escribir pestaña vehiculo por hogar.
+
 
 ##Gerenación de gráficos actividad 4
 
 m <- matrix(1:8, 2, 4, byrow=TRUE);
-pdf('graficos actividad 3.pdf')
+pdf('graficos actividad 3.pdf') #Generación de PDF reporte
 layout(m);
-hist(edad, main="Edad", col="red", xlab="edades", ylab="frecuencia");
-names(porsex) <- c('Varones', 'Mujeres')
-lbls <- paste(names(porsex), porsex)
-lbls <- paste(lbls, "%", sep="")
-pie(porsex, labels=lbls, main="Distribución alumnos", col=rainbow(length(lbls)))
-hist(lic, main="Habitantes Hogar", col="blue", xlab="Alumnos con licencia", ylab="frecuencia")
-hist(phogar, main="Personas por hogar", col="red", xlab="Personas por hogar", ylab="frecuencia")
-hist(lhogar, main="Personas con licencia p/h", col="green", xlab="Personas con licencia p/h", ylab="frecuencia")
-hist(chogar, main="Personas que conducen p/h", col="brown", xlab="Personas que conducen p/h", ylab="frecuencia")
-hist(vhogar, main="Vehiculos motorizados p/h", col="yellow", xlab="Vehiculos motorizados p/h", ylab="frecuencia")
+hist(edad, main="Edad", col="red", xlab="edades", ylab="frecuencia"); #Histograma de edades
+names(porsex) <- c('Mujeres', 'Varones') #Torta edades
+lbls1 <- paste(names(porsex), porsex)
+lbls1 <- paste(lbls1, "%", sep="")
+pie(porsex, labels=lbls1, main="Distribución alumnos", col=rainbow(length(lbls1)))
+names(porlic) <- c('Con licencia','Sin licencia') #Torta licencia
+lbls2 <- paste(names(porlic),porlic);
+lbls2 <- paste(lbls2, "%", sep="");
+pie(porlic, labels=lbls2, main="Distribución licencia", col=rainbow(length(lbls2)))
+hist(phogar, main="Personas por hogar", col="red", xlab="Personas por hogar", ylab="frecuencia") #Histograma personas por hogar.
+hist(lhogar, main="Personas con licencia p/h", col="green", xlab="Personas con licencia p/h", ylab="frecuencia") #Histograma licencias por hogar.
+hist(chogar, main="Personas que conducen p/h", col="brown", xlab="Personas que conducen p/h", ylab="frecuencia") #Histograma conductores por hogar.
+hist(vhogar, main="Vehiculos motorizados p/h", col="yellow", xlab="Vehiculos motorizados p/h", ylab="frecuencia") #Histograma vehiculos motorizados por hogar.
 
 
-} else { cat('Por favor introduzca un valor correcto')}}#Mensaje de error 
+} else { cat('Por favor introduzca un valor correcto')}}#Mensaje de error.
 
 
 
